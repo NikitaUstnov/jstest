5cb0bf8ad3f7e11c02df092ce7459c00c2058f43
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-10-21 01:00:49 +0300

    localStorage

--- test.js
+++ test.js
@@ -1,2 +1,46 @@
-const inpNameRef = document.getElementById('name')
-const inpCommentRef = document.getElementById('comment')
+const inpNameRef = document.querySelector('form')
+const inpCommentRef = document.querySelector('textarea')
+
+// const subBtnEl = document.querySelector('.sub-btn')
+ const resBtnEl = document.querySelector('.res-btn')
+
+inpCommentRef.addEventListener('input', saveInpText)
+inpNameRef.addEventListener('submit', sendTextEl)
+
+savedTextEl()
+
+function saveInpText(evt) {
+    const message = evt.target.value
+    localStorage.setItem('comment-msg', message)
+}
+
+function sendTextEl(evt) {
+    evt.preventDefault()
+    evt.currentTarget.reset()
+    localStorage.removeItem('comment-msg')
+}
+
+function savedTextEl(evt) {
+    const savedText = localStorage.getItem('comment-msg')
+
+    if (savedText) {
+       inpCommentRef.value = savedText
+    }
+    
+}
+
+resBtnEl.addEventListener('click', onResBtnClick)
+
+function onResBtnClick() {
+
+
+    if (confirm("не нажимай сюда больше. напиши OK, ксли понял")) {
+	
+	alert("молодец");
+	}
+else {
+        alert("все с тобой понятно");
+        window.top.close()
+	}
+   
+}
